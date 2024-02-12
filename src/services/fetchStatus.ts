a6842a7cfe55d599963c3719251b8a2becc567f2
Mussin Y <mussin0401@gmail.com>
2024-02-12 21:33:26 +0600

    status extended

--- src/services/fetchStatus.ts
+++ src/services/fetchStatus.ts
@@ -1,14 +1,16 @@
 
-const idBoards = '658c1664242f1f47da07c91e'
+import { STEPS } from "./steps";
+
+const ID_BOARDS = '658c1664242f1f47da07c91e'
 export const fetchStatus = async (phone: string): Promise<string> => {
 
     let query = phone
         .replaceAll(' ', '')
-        .replaceAll('(', '')
-        .replaceAll(')', '')
-        .replaceAll('-', '')
+        // .replaceAll('(', '')
+        // .replaceAll(')', '')
+        // .replaceAll('-', '')
 
-    const url = `${process.env.TRELLO_API_URL}/search?idBoards=${idBoards}&modelTypes=cards&key=${process.env.TRELLO_API_KEY}&token=${process.env.TRELLO_API_TOKEN}&query=name:${query}`
+    const url = `${process.env.TRELLO_API_URL}/search?idBoards=${ID_BOARDS}&modelTypes=cards&key=${process.env.TRELLO_API_KEY}&token=${process.env.TRELLO_API_TOKEN}&query=name:${query}`
     const response = await fetch(url, {
         method: 'GET'
     })
@@ -31,7 +33,12 @@ const fetchListName = async (listId: string): Promise<string> => {
         method: 'GET'
     })
     const result = await response.json()
+    const index:number = parseInt(result.name?.split('.')[0])
+    const step = STEPS[index]
+    const nextStep = STEPS[index + 1]
 
-    const status = `Статус: ${result.name?.split(':')[0]}`
+    const status = `Статус: *${step.name}*\n` + 
+                    `Следующий этап: ${nextStep.name}\n` +
+                    `через: ${step.duration}`
     return status
 }
---
+++ src/services/steps.ts
@@ -0,0 +1,48 @@
+export  const STEPS = [
+  {},
+  {
+      name: 'Менеджер',
+      duration: '3-4 дня',
+      isReady: false
+  },
+  {
+      name: 'Готовится Дизайн',
+      duration: '3-4 дня',
+      isReady: false
+  },
+  {
+      name: 'Готовится Печать',
+      duration: '3-4 дня',
+      isReady: false
+  },
+  {
+      name: 'Готовятся Колокольчики',
+      duration: '3-4 дня',
+      isReady: false
+  },
+  {
+      name: 'Готовятся Стразы',
+      duration: '3-4 дня',
+      isReady: false
+  },
+  {
+      name: 'У швей',
+      duration: '3-4 дня',
+      isReady: false
+  },
+  {
+      name: 'На контроле качества',
+      duration: '3-4 дня',
+      isReady: false
+  },
+  {
+      name: 'Этап фото-отчета',
+      duration: '3-4 дня',
+      isReady: false
+  },
+  {
+      name: 'Доставка',
+      duration: '3-4 дня',
+      isReady: false
+  },
+]
