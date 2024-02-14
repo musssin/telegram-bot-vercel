40649d092a367beedba2793993b345ab72361a04
Mussin Y <mussin0401@gmail.com>
2024-02-14 21:01:39 +0600

    leave

--- src/scenes/statusScene.ts
+++ src/scenes/statusScene.ts
@@ -5,16 +5,21 @@ import { Message } from 'telegraf/typings/core/types/typegram';
 const debug = createDebug('bot:about_command');
 
 const { leave } = Scenes.Stage;
-
+let count = 0
 const checkStatus = () => async (ctx: Context) => {
+  if (count > 1) {
+    leave<Scenes.SceneContext>()
+    return
+  }
+  count++
+  
   const message = ctx.message as Message.TextMessage
   debug(`Triggered "checkStatus" command`);
   const result = await fetchStatus(message.text)
 
   await ctx.replyWithMarkdownV2(result, { parse_mode: 'Markdown' })
 
-  leave<Scenes.SceneContext>()
-  statusScene.leave()
+  
 
 }
 
--- src/scenes/subscribeScene.ts
+++ src/scenes/subscribeScene.ts
@@ -5,16 +5,19 @@ import { setCardChatId } from '../services/setCardChatId';
 const debug = createDebug('bot:about_command');
 
 const { leave } = Scenes.Stage;
-
+const count = 0
 const subscribePhone = () => async (ctx: Context) => {
 
+  if(count > 1) {
+    leave<Scenes.SceneContext>()
+    return
+  }
   const message = ctx.message as Message.TextMessage
   const result = await setCardChatId(message.text, message.chat.id.toString())
 
   await ctx.replyWithMarkdownV2(result, { parse_mode: 'Markdown' })
 
   leave<Scenes.SceneContext>()
-  subscribeScene.leave()
 }
 
 const subscribeScene = new Scenes.BaseScene<Scenes.SceneContext>("subscribeScene");
