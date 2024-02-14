427d2b53a728ca7d1c929987807ff6f3beb7eb4d
yernat.mussin <yernat.mussin@iserv.kz>
2024-02-14 16:53:20 +0600

    trello removed logs

--- src/webhooks/trello.ts
+++ src/webhooks/trello.ts
@@ -15,28 +15,24 @@ export const trelloWebHook = async (req: any, res: any) => {
 
         const actionType = req.body?.action?.display?.translationKey;
         if (actionType !== ACTION_MOVING_CARD) return
-        bot.telegram.sendMessage('812794909', 'key');
         const cardShort = req.body?.action?.display?.entities?.card;
         const card = await fetchCardById(cardShort.id)
         if (!card) return
 
-        bot.telegram.sendMessage('812794909', 'fetched');
-        bot.telegram.sendMessage('812794909', JSON.stringify(card));
         const changedStatusIndex = parseInt(req.body?.action?.display?.entities?.listAfter?.text?.split('.')[0]);
         const changedStatus = STEPS[changedStatusIndex]
-        const changedStatusNext = STEPS[changedStatusIndex]
+        const changedStatusNext = STEPS[changedStatusIndex + 1]
 
         const chatField = card.customFieldItems?.find((customField: any) => customField.idCustomField === CHAT_FIELD_ID)
         const chatId = chatField?.value?.text
         if (!chatId) return
-        bot.telegram.sendMessage('812794909', 'finded');
+
         const message = 'Статус заказа изменился на: *' + changedStatus.name + '*\n' +
             'займет ' + changedStatus.duration + '\n' +
             'Следующий этап: ' + changedStatusNext.name;
 
         bot.telegram.sendMessage(chatId, message);
 
-        res.sendStatus(200);
     } catch (error: any) {
         bot.telegram.sendMessage('812794909', error?.message?.toString());
     }
